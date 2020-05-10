432041761dd00bddad55edb14afac77a5ae29eff
辣鸡土豆 <894039627@qq.com>
2020-05-10 15:39:58 +0800

    up my code

---
+++ 炉石一键拔线/炉石一键拔线/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("微软雅黑", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button1.Location = New System.Drawing.Point(778, 160)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "初始化"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(778, 98)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(58, 39)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "断开"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(254, 162)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(455, 25)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "D:\Program Files (x86)\Hearthstone\Hearthstone.exe"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(842, 98)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(58, 39)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "连接"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(778, 14)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(122, 69)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "拔线"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(32, 158)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(192, 27)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "你的炉石传说地址："
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(37, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(648, 81)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "使用说明：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "第一次打开，请输入炉石传说地址，然后点击初始化。" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "每次点击拔线就会自动断开连接，然后3s后连接。(快捷键qwer一起按）"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3000
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(967, 216)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "炉石传说一键拔线 by土豆dd"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ 炉石一键拔线/炉石一键拔线/Form1.vb
@@ -0,0 +1,90 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim rootdir As String = ""
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Integer) As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim LSdir As String
+
+        Dim blocktxt As String =
+            "%1 mshta vbscript:CreateObject(""Shell.Application"").ShellExecute(""cmd.exe"",""/c %~s0 ::"","""",""runas"",0)(window.close)&&exit" & vbNewLine &
+            "C:\Windows\System32\netsh advfirewall firewall set rule name=""lushi"" new enable=yes"
+        Dim connecttxt As String =
+            "%1 mshta vbscript:CreateObject(""Shell.Application"").ShellExecute(""cmd.exe"",""/c %~s0 ::"","""",""runas"",0)(window.close)&&exit" & vbNewLine &
+            "C:\Windows\System32\netsh advfirewall firewall set rule name=""lushi"" new enable=no"
+        Dim Swriter1 As New StreamWriter(rootdir & "block.bat", False) '覆盖或新建
+        Swriter1.WriteLine(blocktxt)
+        Swriter1.Close()
+        Dim Swriter2 As New StreamWriter(rootdir & "connect.bat", False) '覆盖或新建
+        Swriter2.WriteLine(connecttxt)
+        Swriter2.Close()
+
+
+        LSdir = TextBox1.Text.Trim()
+        Dim setuptxt As String =
+            "%1 mshta vbscript:CreateObject(""Shell.Application"").ShellExecute(""cmd.exe"",""/c %~s0 ::"","""",""runas"",1)(window.close)&&exit" & vbNewLine &
+            "C:\Windows\System32\netsh advfirewall firewall delete rule name=""lushi"" " & vbNewLine &
+            "C:\Windows\System32\netsh advfirewall firewall add rule name=""lushi"" dir=out action=block program=""" & LSdir & """ enable=no"
+        Dim Swriter As New StreamWriter(rootdir & "setup.bat", False) '覆盖或新建
+        Swriter.WriteLine(setuptxt)
+        Swriter.Close()
+        Shell("setup.bat", vbHidden)
+        MsgBox("初始化完成！")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Shell(rootdir & "connect.bat")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim oShell
+        oShell = CreateObject("WScript.Shell")
+        rootdir = oShell.ExpandEnvironmentStrings("%APPDATA%")
+        'MsgBox(rootdir)
+        If Not Directory.Exists(rootdir & "\HSunplugging") Then
+            Directory.CreateDirectory(rootdir & "\HSunplugging")
+        End If
+        rootdir = rootdir & "\HSunplugging\"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Shell(rootdir & "block.bat", vbHidden)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Shell(rootdir & "block.bat", vbHidden)
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Shell(rootdir & "connect.bat", vbHidden)
+        Timer1.Enabled = False
+        Timer2.Enabled = True
+    End Sub
+
+    Function GetKey() As String
+        Dim KeyResultQ, KeyResultW, KeyResultE, KeyResultR As Integer
+        KeyResultQ = GetAsyncKeyState(Asc("Q"))
+        KeyResultW = GetAsyncKeyState(Asc("W"))
+        KeyResultE = GetAsyncKeyState(Asc("E"))
+        KeyResultR = GetAsyncKeyState(Asc("R"))
+        '-32768（即道16进制数&H8000） --- 键当前处于按下状态，但在此之前（自上次调用GetAsyncKeyState后）键未被按过
+        '但是int里是32769
+        'And KeyResultW = 32769 And KeyResultE = 32769 And KeyResultR = 32769 
+        If KeyResultQ <> 0 And KeyResultW <> 0 And KeyResultE <> 0 And KeyResultR <> 0 Then
+            Return 1
+        End If
+        Return 0
+    End Function
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim test = GetKey()
+
+        If test = 1 Then
+            Timer2.Enabled = False
+            Shell(rootdir & "block.bat", vbHidden)
+            Timer1.Enabled = True
+        End If
+    End Sub
+End Class
